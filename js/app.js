5c1a7929f2e80313dcd135df2deb7bb916ebbaf1
Allams <anthony.allams@gmail.com>
2020-07-18 11:50:46 +0100

    Added countdown for hngi8 and missing popup image

--- js/app.js
+++ js/app.js
@@ -102,3 +102,60 @@ form.addEventListener("submit", (e) => {
 // };
 
 // init();
+
+///////////////////////////////////
+///////////////////////////////////
+//COUNTDOWN TIMER FUNCTION
+///////////////////////////////////
+//////////////////////////////////
+const days = document.querySelector(".days");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const year = document.querySelector(".year");
+const countdown = document.querySelector(".countdown");
+
+//Declare the time variables
+const currentYear = new Date().getUTCFullYear();
+const eventMonth = `January`.slice(0, 3);
+const eventDay = `26`;
+let eventDate = new Date(`${eventMonth} ${eventDay} ${currentYear} 00:00:00`);
+let diff;
+
+//Get event date based on current date
+const getEventDate = () => {
+  const time = new Date();
+  if (eventDate > time) {
+    //Set background year
+    year.innerHTML = `${eventMonth} ${eventDay}, ${currentYear}`;
+  } else {
+    eventDate = new Date(
+      `${eventMonth} ${eventDay} ${currentYear + 1} 00:00:00`
+    );
+    //Set background year
+    year.innerHTML = `${eventMonth} ${eventDay}, ${currentYear + 1}`;
+  }
+};
+
+//Pad hours, mins and secs with 0 if value is less than 10
+const padTime = (time) => time.toString().padStart(2, "00");
+
+//Update the DOM Count down
+const updateDOMCountDown = () => {
+  const time = new Date();
+  const diff = eventDate - time;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  //Add the time values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = padTime(h);
+  minutes.innerHTML = padTime(m);
+  seconds.innerHTML = padTime(s);
+};
+getEventDate();
+//Run the time every second
+setInterval(updateDOMCountDown, 1000);
