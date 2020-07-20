524817042b347e9e3b3f4ac4aabdc357df227d48
Allams <anthony.allams@gmail.com>
2020-07-20 01:00:35 +0100

    Adjusted proloader time to 1.5s

--- js/app.js
+++ js/app.js
@@ -94,7 +94,7 @@ $("#back-to-top").click(function () {
 
 //Preloader;
 $(window).on("load", function () {
-  $(".status").slideUp(1000);
+  $(".status").slideUp(1500);
 });
 
 // const loader = document.querySelector(".loader");
